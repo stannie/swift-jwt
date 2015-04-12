f51e070c592a6e5bdd6e8682a7104b0ff5d1d36a
Stan P. van de Burgt <stan@vandeburgt.com>
2015-04-11 23:14:33 -0700

    project and workspace files

---
+++ JWT/CommonCrypto/CommonCrypto.h
@@ -0,0 +1,19 @@
+//
+//  CommonCrypto.h
+//  CommonCrypto
+//
+//  Created by Stan P. van de Burgt on 3/31/15.
+//  Copyright (c) 2015 RoundZero bv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonCrypto.
+FOUNDATION_EXPORT double CommonCryptoVersionNumber;
+
+//! Project version string for CommonCrypto.
+FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCrypto/PublicHeader.h>
+
+

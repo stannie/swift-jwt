79860c6536a67099cc26c8005a3db974339ef5fc
Stan P. van de Burgt <stan@vandeburgt.com>
2015-04-12 11:55:56 -0700

    header file

---
+++ JWT/JWT/SwiftJWT.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJWT.h
+//  JWT class
+//
+//  Created by Stan P. van de Burgt on 3/31/15.
+//  Copyright (c) 2015 RoundZero bv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJWT.
+FOUNDATION_EXPORT double SwiftJWTVersionNumber;
+
+//! Project version string for SwiftJWT.
+FOUNDATION_EXPORT const unsigned char SwiftJWTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWT/PublicHeader.h>
+
+
